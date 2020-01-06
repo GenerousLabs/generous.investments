fe0d1992ff28bbb40e5603d9d4141fe367d22f15
Callum Macdonald <git@callum-macdonald.com>
2020-01-06 18:35:11 +0100

    Stop breaking the blog page.

--- gatsby/src/pages/blog.js
+++ gatsby/src/pages/blog.js
@@ -3,11 +3,17 @@ import React from 'react';
 import { Layout, SEO } from 'components';
 import { BlogPostList } from 'containers';
 
+const BlogPage = () => {
+  return <div />;
+};
+
+/*
 const BlogPage = () => (
   <Layout noBackground>
     <SEO title='Blog' />
     <BlogPostList />
   </Layout>
 );
+*/
 
 export default BlogPage;
