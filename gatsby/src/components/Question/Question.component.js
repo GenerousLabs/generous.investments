1f43fb4447a65d891e8b30a6652c4c7503918ec6
Callum Macdonald <git@callum-macdonald.com>
2020-01-10 18:42:35 +0100

    Replace choices with "change your mind".

--- gatsby/src/components/Question/Question.component.js
+++ gatsby/src/components/Question/Question.component.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
 
 const filterChildren = (answer, children) => {
   // Before there is an answer, render nothing
@@ -13,6 +14,7 @@ const filterChildren = (answer, children) => {
 };
 
 export const Question = ({ children }) => {
+  const classes = useStyles();
   const [answer, setAnswer] = useState(null);
 
   const hasAnswer = typeof answer === 'boolean';
@@ -21,28 +23,48 @@ export const Question = ({ children }) => {
 
   return (
     <div style={{ marginBottom: 600 }}>
-      <p>
-        <Button
-          variant='contained'
-          color={hasAnswer && answer ? 'primary' : 'default'}
-          onClick={() => {
-            setAnswer(true);
-          }}>
-          Yes
-        </Button>
-        <Button
-          variant='contained'
-          color={hasAnswer && !answer ? 'primary' : 'default'}
-          onClick={() => {
-            setAnswer(false);
-          }}>
-          No
-        </Button>
-      </p>
+      {hasAnswer ? (
+        <p>
+          <Button
+            variant='outlined'
+            size='small'
+            classes={{ label: classes.changeYourMind }}
+            onClick={() => {
+              setAnswer(null);
+            }}>
+            Change your mind?
+          </Button>
+        </p>
+      ) : (
+        <p>
+          <Button
+            variant='contained'
+            color={hasAnswer && answer ? 'primary' : 'default'}
+            onClick={() => {
+              setAnswer(true);
+            }}>
+            Yes
+          </Button>
+          <Button
+            variant='contained'
+            color={hasAnswer && !answer ? 'primary' : 'default'}
+            onClick={() => {
+              setAnswer(false);
+            }}>
+            No
+          </Button>
+        </p>
+      )}
       {filteredChildren}
     </div>
   );
 };
+
+const useStyles = makeStyles(() => ({
+  changeYourMind: {
+    fontSize: '0.8em',
+  },
+}));
 
 export const Yes = ({ children }) => {
   return children;
